913398c86c49e918738820c862bbbbc3c9c7d2cf
ebchandelier <ebchandelier@gmail.com>
2017-05-25 10:58:54 -0300

    nosso exemplo
    colocar em exemplos e chamar com make nossoExemplo

---
+++ nossoExemplo.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<errno.h>
+#include "../include/support.h"
+#include "../include/cthread.h"
+#include <stdio.h>
+
+
+int main() {
+
+return 0;
+}
