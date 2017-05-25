c0e87cf0102025d11679c7c9c37dde42d5842298
brunoloureiro <brunoloureiro@users.noreply.github.com>
2017-05-25 19:31:17 -0300

    cidentify

---
+++ testes/nossoExemplo4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "../include/support.h"
+#include "../include/cthread.h"
+
+int main(int argc, char *argv[ ]) {
+	char str1[300];
+	char str2[25];
+	int ret_id;
+
+	ret_id = cidentify(str1, 300);
+	printf("ret %d -- %s\n", ret_id, str1);
+
+	ret_id = cidentify(str2, 300);
+	printf("ret %d -- %s\n", ret_id, str2);
+
+
+}
